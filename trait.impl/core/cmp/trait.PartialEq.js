b7400dbb0f4bcf6ee826ee146bb484814261c805
andrehrferreira <andrehrferreira@users.noreply.github.com>
2026-04-19 06:34:22 +0000

    deploy: 8be87a1e712fe3a60e8c02595aaf2e45dc404d5f

--- trait.impl/core/cmp/trait.PartialEq.js
+++ trait.impl/core/cmp/trait.PartialEq.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hive_gpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"hive_gpu/backends/detector/enum.GpuBackendType.html\" title=\"enum hive_gpu::backends::detector::GpuBackendType\">GpuBackendType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"hive_gpu/traits/enum.BufferType.html\" title=\"enum hive_gpu::traits::BufferType\">BufferType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"hive_gpu/types/enum.GpuDistanceMetric.html\" title=\"enum hive_gpu::types::GpuDistanceMetric\">GpuDistanceMetric</a>"]]]]);
+    const implementors = Object.fromEntries([["hive_gpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"hive_gpu/backends/detector/enum.GpuBackendType.html\" title=\"enum hive_gpu::backends::detector::GpuBackendType\">GpuBackendType</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"hive_gpu/traits/enum.BufferType.html\" title=\"enum hive_gpu::traits::BufferType\">BufferType</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"hive_gpu/types/enum.GpuDistanceMetric.html\" title=\"enum hive_gpu::types::GpuDistanceMetric\">GpuDistanceMetric</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[902]}
+//{"start":59,"fragment_lengths":[908]}
--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hive_gpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"hive_gpu/traits/enum.BufferType.html\" title=\"enum hive_gpu::traits::BufferType\">BufferType</a>"]]]]);
+    const implementors = Object.fromEntries([["hive_gpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"hive_gpu/traits/enum.BufferType.html\" title=\"enum hive_gpu::traits::BufferType\">BufferType</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[279]}
+//{"start":59,"fragment_lengths":[281]}
